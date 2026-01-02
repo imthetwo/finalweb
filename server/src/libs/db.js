dbdd932916d07c6aa62d19ba2659a6124d780f0e
imthetwo <hungcocacola2132004@gmail.com>
2026-01-02 15:01:25 -0800

    structure xog

---
+++ server/src/libs/db.js
@@ -0,0 +1,11 @@
+import mongoose, { mongo } from "mongoose"
+
+export const connectDB = async () => {
+    try{
+        await mongoose.connect(process.env.MONGODB_CONNECTIONSTRING)
+        console.log('Connected Database')
+    } catch (error){
+        console.log('Can not connect to DB:',error)
+        process.exit(1);
+    }
+}
---
+++ server/src/models/User.js
@@ -0,0 +1,62 @@
+import mongoose from "mongoose";
+
+const userSchemas= new mongoose.Schema({
+    username:{
+        type : String,
+        required:true,
+        unique:true,
+        trim:true,
+        lowercase:true
+    },
+
+    hashedPassword:{ 
+        type:String,
+        required:true,
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+        lowercase:true,
+    },
+
+    displayName:{
+        type:String,
+        required:true,
+        trim:true,
+    },
+
+    avartarUrl:{
+        type:String, //link cdn để hiện thị hình
+    },
+
+    avatarId:{
+        type:String, // cloudinary public_id để xóa hình
+    },
+
+    bio:{
+        type:String,
+        maxlength:500, // tùy
+    },
+
+    phone:{
+        type:String,
+        sparse:true, // cho phép null,nhưng không được trùng
+    },
+    createdAt:
+    { 
+    timestamps:true
+    },
+});
+
+const User = mongoose.model("User" , userSchemas);
+export default User;
+
+    
+    
+
+
+
+
+
+
